bd7bb33697325fed105ab246d5ccf628f25db634
Jeremiah Whitehurst <101281773+Jeremiah-A-WV@users.noreply.github.com>
2022-03-09 18:29:20 +0000

    Initial commit

---
+++ Calculator.java
@@ -0,0 +1,32 @@
+/* File Name: Calculator.java
+Entire Code By: Jeremiah Whitehurst */
+
+class Calculator {
+  Calculator() {}
+
+  // adds the parameters together
+  public static int add(int a, int b) {
+    return (a + b);
+  }
+
+  // subtracts the second parameter from the first one
+  public static int subtract(int a, int b) {
+    return (a - b);
+  }
+
+  // divides the first parameter by the second one
+  public static int divide(int a, int b) {
+    return (a / b);
+  }
+
+  // multiplies the first parameter by the second one
+  public static int multiply(int a, int b) {
+    return (a * b);
+  }
+
+  // use escape sequence to clear the console, and then use the flush method to get rid of the remaining bytes
+  public static void clearScreen() {
+    System.out.print("\033[H\033[2J");  
+    System.out.flush();
+  }
+}
---
+++ Main.java
@@ -0,0 +1,54 @@
+/* File Name: Main.java
+Entire Code By: Jeremiah Whitehurst */
+
+// import the scanner
+import java.util.Scanner;
+
+// main class extend calculator class
+public class Main extends Calculator {
+  public static void main(String[] args) {
+    // calculator object allows usage of the operational methods and the clear screen method
+    Calculator calc = new Calculator();
+
+    // scanner for user input
+    Scanner sc = new Scanner(System.in);
+
+    // clear screen first to get rid of clutter
+    calc.clearScreen();
+    
+    // request user's chosen numbers, then clears the console for the next prompt
+    System.out.println("Enter your numbers in order: ");
+    int a, b;
+    a = sc.nextInt();
+    b = sc.nextInt();
+    calc.clearScreen();
+
+    // request user's chosen operator
+    System.out.println("Select your operator (+, -, /, *): ");
+    char choice;
+
+    // selects the first character in whatever the user types, then clear the screen to display the answer
+    choice = sc.next().charAt(0);
+    System.out.flush();
+    calc.clearScreen();
+
+    // the user's chosen operator decides what method is used, respectively
+    switch(choice) {
+      case '+':
+        System.out.println(a + " plus " + b + " equals " + calc.add(a, b));
+        break;
+      case '-':
+        System.out.println(a + " minus " + b + " equals " + calc.subtract(a, b));
+        break;
+      case '/':
+        System.out.println(a + " divided by " + b + " equals " + calc.divide(a, b));
+        break;
+      case '*':
+        System.out.println(a + " multiplied by " + b + " equals " + calc.multiply(a, b));
+        break;
+      default:
+        // if one of the given operators is not given, print an error message
+        System.out.println("Invalid or illegal operation");
+    }
+  }
+}
